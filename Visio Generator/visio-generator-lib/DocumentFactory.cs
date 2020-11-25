a56c42943914f5dc1cbaf9345ab24e7385543dc9
Chris Roth <visioguy@hotmail.com>
2020-11-25 17:36:13 +0100

    Created solution

---
+++ Visio Generator/visio-generator-lib/DocumentFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visio_generator_lib
+{
+    public class DocumentFactory
+    {
+        // Not sure if we can create a packet from scratch...
+
+        // The default blank drawing (U.S. Visio) has this:
+        // /
+        //  docProps
+        //      app.xml
+        //      core.xml
+        //      custom.xml
+        //      thumbnail.emf
+        //  visio
+        //      document.xml
+        //      pages
+        //          page1.xml
+        //          pages.xml
+        //          _rels
+        //              pages.xml.rels
+        //      windows.xml
+        //  _rels
+        //      document.xml.rels
+        //  _rels
+        //      .rels
+
+
+    }
+}
